0e199a07e665b5dff98fe21e1ac4e8414f8cff5c
ayoline <ayo@outlook.com.br>
2021-07-05 21:15:16 -0300

    first commit!

---
+++ lib/AlcoolGasolina.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AlcoolGasolina extends StatefulWidget {
+  const AlcoolGasolina({Key? key}) : super(key: key);
+
+  @override
+  _AlcoolGasolinaState createState() => _AlcoolGasolinaState();
+}
+
+class _AlcoolGasolinaState extends State<AlcoolGasolina> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'AlcoolGasolina.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: AlcoolGasolina(),
+  ));
+}
